a0cd45b06cf7c70939942158a82f5a7a5c5881a4
Andrew Kaiser <akaiser@wayfair.com>
2018-06-08 14:17:29 -0400

    cleanup schema
    - make toplevel url builder, then nest into recursing scraper types

--- tests/tumblr/config.js
+++ tests/tumblr/config.js
@@ -1,22 +1,21 @@
 export default {
   input: 'username',
-  next_url: {
-    type: 'increment',
-    build_url:
-      'https://{artist}.deviantart.com/gallery/?catpath=/&offset={_page}',
-    increment_by: 50,
-    regex_cleanup: {
-      select: 'var tumblr_api_read = ',
-      replace: ''
+  scrape: {
+    build_url: {
+      increment: true,
+      increment_by: 50,
+      url_template:
+        'https://{username}.tumblr.com/api/read/json?start={_index}&num=50',
+      regex_cleanup: 'var tumblr_api_read = ',
+      expect: 'json'
     },
-    expect: 'json'
-  },
-  scrape_criteria: {
-    criteria: {
-      selector: 'posts[type:photo].photo-url-1280',
-      regex_cleanup: {
-        select: 'https://d+?.',
-        replace: 'https://'
+    for_each: {
+      criteria: {
+        selector: 'posts[type:photo].photo-url-1280',
+        regex_cleanup: {
+          select: 'https://d+?.',
+          replace: 'https://'
+        }
       }
     }
   }
