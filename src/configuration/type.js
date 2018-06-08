43323bf1fc0e479caf0f74d90aa3febbe5e10b93
Andrew Kaiser <kaisea.rpi@gmail.com>
2018-06-08 14:52:28 -0400

    make config type

--- src/configuration/type.js
+++ src/configuration/type.js
@@ -1,53 +1,53 @@
 type RegexRemove = string
-type RegexReplace = {
+type RegexReplace = {|
   select: string,
   replace?: string, // defaults to ''
   flags?: string // defaults to 'g'
-}
+|}
 type Regex = RegexRemove | RegexReplace
 
 type ExpectedResults = 'html' | 'json' | 'scalar'
 
 // User input
 type InputSimple = string
-type InputCleaned = {
+type InputCleaned = {|
   name: string,
   regex_cleanup: Regex
-}
+|}
 type Input = InputSimple | InputCleaned
 
 // html or json parser
-type Scrape = {
+type Scrape = {|
   selector: string, // html selector (e.g. '.someclass > span')
   attribute?: string, // html element attribute (e.g. src), if not specified w/ html, textContent is selected
   regex_cleanup?: Regex,
   expect?: ExpectedResults
-}
+|}
 
 // recursing parser
-type ScrapeCriteria = {
+type ScrapeCriteria = {|
   criteria: Scrape,
   for_each?: ScrapeCriteria
-}
+|}
 
-type NextUrlBase = {
+type NextUrlBase = {|
   url_template: string,
   regex_cleanup?: Regex,
   expect?: ExpectedResults
-}
-type NextUrlIncrement = {
+|}
+type NextUrlIncrement = {|
   ...NextUrlBase,
   increment: boolean, // defaults to false
   initial_index?: number, // defaults to 0
   increment_by?: number // defaults to 1
-}
+|}
 
 type NextUrl = NextUrlBase | NextUrlIncrement
 
-export type Config = {
+export type Config = {|
   input: Input | [Input],
-  scrape: {
+  scrape: {|
     build_url: NextUrl,
     for_each: ScrapeCriteria
-  }
-}
+  |}
+|}
