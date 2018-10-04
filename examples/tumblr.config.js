43ebbfbf4ab129b25f76e4e6e7b9982079be552d
Andrew Kaiser <kaisea.rpi@gmail.com>
2018-10-04 11:54:55 -0400

    fix type change

--- examples/tumblr.config.js
+++ examples/tumblr.config.js
@@ -4,33 +4,37 @@ export default {
     download: {
       increment: 50,
       urlTemplate:
-        'https://{username}.tumblr.com/api/read/json?start={_index}&num=50',
-      regexCleanup: 'var tumblr_api_read = '
+        'https://{username}.tumblr.com/api/read/json?start={_index}&num=50'
     },
-    parse: {
-      expect: 'json',
-      selector: 'posts[type:photo]'
-    },
-    scrapeEach: [
-      {
-        name: 'image',
-        parse: {
-          selector: 'photo-url-1280',
-          regexCleanup: {
-            select: 'https://d+?.',
-            replace: 'https://'
+    // TODO add this to type
+    // regexCleanup: 'var tumblr_api_read = ',
+    scrapeEach: {
+      name: 'parse-json',
+      parse: {
+        expect: 'json',
+        selector: 'posts[type:photo]'
+      },
+      scrapeEach: [
+        {
+          name: 'image',
+          parse: {
+            selector: 'photo-url-1280',
+            regexCleanup: {
+              select: 'https://d+?.',
+              replace: 'https://'
+            }
+          },
+          scrapeEach: {
+            download: '{value}'
           }
         },
-        scrapeEach: {
-          download: '{value}'
+        {
+          name: 'tags',
+          parse: {
+            selector: 'tags'
+          }
         }
-      },
-      {
-        name: 'tags',
-        parse: {
-          selector: 'tags'
-        }
-      }
-    ]
+      ]
+    }
   }
 }
