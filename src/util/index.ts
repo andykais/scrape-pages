0a6c21bb838331f5923f7c969fdfe9c0942dd931
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-05-15 13:48:22 -0400

    refactor: move external utils to util/ dir

---
+++ src/util/index.ts
@@ -0,0 +1,22 @@
+/**
+ * This is not an internal util file, it only exists for external use.
+ */
+import { normalizeConfig, flattenConfig } from '../settings/config'
+import { normalizeOptions } from '../settings/options'
+import { normalizeParams } from '../settings/params'
+import { typecheckConfig, typecheckOptions, typecheckParams } from './typechecking.runtime'
+
+export const config = {
+  normalize: normalizeConfig,
+  flatten: flattenConfig,
+  typecheck: typecheckConfig
+}
+export const options = {
+  normalize: normalizeOptions,
+  typecheck: typecheckOptions
+}
+
+export const params = {
+  normalize: normalizeParams,
+  typecheck: typecheckParams
+}
