9dcd362ef428c9874c80db14daab1bb4ba65c554
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-05-16 01:19:10 -0400

    feature: add json parse support

--- src/scraper/scrape-step/parser/implementations/json.ts
+++ src/scraper/scrape-step/parser/implementations/json.ts
@@ -1,7 +1,23 @@
+import { JSONPath } from 'jsonpath-plus'
 import { AbstractParser } from '../abstract'
+// type imports
+import { ParseConfigJson } from '../../../../settings/config/types'
 
 export class Parser extends AbstractParser {
   public type: 'json' = 'json'
+  protected parseConfig: ParseConfigJson
 
-  public parse = (value: string) => [value]
+  public parse = (value: string) => {
+    const object = JSON.parse(value)
+
+    const result = JSONPath({
+      path: this.parseConfig.selector
+    })
+
+    return Array.isArray(result)
+      ? result.map(val => (typeof val === 'object' ? JSON.stringify(val) : val.toString()))
+      : typeof result === 'object'
+      ? [JSON.stringify(result)]
+      : [result.toString()]
+  }
 }
--- tsconfig/custom.d.ts
+++ tsconfig/custom.d.ts
@@ -2,3 +2,7 @@ declare module '*.sql' {
   const content: string
   export default content
 }
+
+declare module 'jsonpath-plus' {
+  export const JSONPath: any
+}
