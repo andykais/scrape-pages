cc1e1d1fc858eda5a40aa264fedea4858be08413
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-05-16 09:14:54 -0400

    test: add json parse test

--- testing/functional/json-parsing/config.ts
+++ testing/functional/json-parsing/config.ts
@@ -2,6 +2,12 @@ import { ConfigInit } from '../../../src/settings/config/types'
 
 export const config: ConfigInit = {
   scrapers: {
+    jsonInJs: {
+      download: {
+        urlTemplate: 'http://json-parsing.com/json-inside-js-response.js',
+        regexCleanup: 'var api_read = '
+      }
+    },
     apiResponse: {
       download: 'http://json-parsing.com/api-response.json',
       parse: {
@@ -13,52 +19,5 @@ export const config: ConfigInit = {
   },
   run: {
     scraper: 'apiResponse'
-  }
-}
-
-export const configParseJsonTwice: ConfigInit = {
-  scrapers: {
-    parsePosts: {
-      download: 'http://json-parsing.com/api-response.json',
-      parse: {
-        format: 'json',
-        selector: `posts[?(@.type=='post')]`
-      }
-    },
-    parseContentFromPost: {
-      parse: {
-        format: 'json',
-        selector: 'content',
-        regexCleanup: '\n$'
-      }
-    }
-  },
-  run: {
-    scraper: 'parsePosts',
-    forEach: {
-      scraper: 'parseContentFromPost'
-    }
-  }
-}
-
-export const configParseJsonInsideScript: ConfigInit = {
-  scrapers: {
-    jsonInJs: {
-      download: {
-        urlTemplate: 'http://json-parsing.com/json-inside-js-response.js.txt',
-        regexCleanup: {
-          selector: 'var api_read = (.*);',
-          replacer: '$1',
-          flags: 's'
-        }
-      },
-      parse: {
-        format: 'json',
-        selector: 'a.nested.story[*].word'
-      }
-    }
-  },
-  run: {
-    scraper: 'jsonInJs'
   }
 }
--- testing/functional/json-parsing/functional.test.ts
+++ testing/functional/json-parsing/functional.test.ts
@@ -3,8 +3,8 @@ import * as path from 'path'
 
 import { expect } from 'chai'
 
-import { NockFolderMock, stripResult } from '../../setup'
-import { config, configParseJsonTwice, configParseJsonInsideScript } from './config'
+import { NockFolderMock, configureSnapshots, stripResult } from '../../setup'
+import { config } from './config'
 import { scrape } from '../../../src'
 
 const resourceFolder = `${__dirname}/fixtures`
@@ -16,6 +16,10 @@ const params = {
   cleanFolder: true
 }
 describe(__filename, () => {
+  beforeEach(function() {
+    configureSnapshots({ __dirname, __filename, fullTitle: this.currentTest!.fullTitle() })
+  })
+
   describe('with simple json response', () => {
     const { start, query } = scrape(config, options, params)
     before(async () => {
@@ -27,48 +31,7 @@ describe(__filename, () => {
     })
     it('should get em', () => {
       const result = query({ scrapers: ['apiResponse'] })
-      expect(result.map(r => r.map(c => c.parsedValue))).to.be.deep.equal([
-        ['the', 'quick', 'brown', 'fox']
-      ])
       expect(stripResult(result)).to.matchSnapshot()
-    })
-  })
-
-  describe('with json blob parsed nested', () => {
-    const { start, query } = scrape(configParseJsonTwice, options, params)
-    before(async () => {
-      const siteMock = await NockFolderMock.create(resourceFolder, resourceUrl)
-      const { on } = await start()
-      await new Promise(resolve => on('done', resolve))
-      siteMock.done()
-    })
-    it('should stringify, then parse, then stringify', () => {
-      const result = query({ scrapers: ['parseContentFromPost'], groupBy: 'parseContentFromPost' })
-      expect(result).to.have.length(4)
-      expect(result.map(r => r.map(c => c.parsedValue))).to.be.deep.equal([
-        ['the'],
-        ['quick'],
-        ['brown'],
-        ['fox']
-      ])
-      expect(stripResult(result)).to.matchSnapshot()
-    })
-  })
-
-  describe('with json that needs to be parsed out of a file', () => {
-    const { start, query } = scrape(configParseJsonInsideScript, options, params)
-    before(async () => {
-      const siteMock = await NockFolderMock.create(resourceFolder, resourceUrl)
-      const { on } = await start()
-      await new Promise(resolve => on('done', resolve))
-      siteMock.done()
-    })
-    it('should stringify, then parse, then stringify', () => {
-      const result = query({ scrapers: ['jsonInJs'] })
-      expect(result[0]).to.have.length(9)
-      expect(result.map(r => r.map(c => c.parsedValue))).to.be.deep.equal([
-        ['the', 'quick', 'brown', 'fox', 'jumped', 'over', 'the', 'lazy', 'dog']
-      ])
     })
   })
 })
