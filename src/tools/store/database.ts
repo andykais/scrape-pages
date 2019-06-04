7990dc7d55316421f7344162713e26fccf21d5a1
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-06-04 16:54:56 -0400

    test: calling query() before start() throws a meaningful error

--- src/tools/store/database.ts
+++ src/tools/store/database.ts
@@ -3,7 +3,9 @@ import * as BetterSqlite3Database from 'better-sqlite3'
 
 class Database extends BetterSqlite3Database {
   public constructor(folder: string) {
-    super(resolve(folder, 'store.sqlite'))
+    super(Database.getFilePath(folder))
   }
+
+  public static getFilePath = (folder: string) => resolve(folder, 'store.sqlite')
 }
 export { Database }
