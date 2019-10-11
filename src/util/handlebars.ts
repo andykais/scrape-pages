5d98e45054f15dd36a2805348bb3c1805ec81b72
Andrew Kaiser <kaisea.rpi@gmail.com>
2019-10-10 22:31:58 -0400

    docs: add explanation for math operators in templates\n(I got confused)

--- src/util/handlebars.ts
+++ src/util/handlebars.ts
@@ -1,5 +1,7 @@
 import * as Handlebars from 'handlebars'
 
+// these are nicities so that inside configs we can do simple math expressions
+// this can be particularly useful when incrementing download urls
 Handlebars.registerHelper('+', (x: number, y: number) => x + y)
 Handlebars.registerHelper('-', (x: number, y: number) => x - y)
 Handlebars.registerHelper('*', (x: number, y: number) => x * y)
